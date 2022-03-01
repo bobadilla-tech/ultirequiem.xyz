1950d420b30942116b4a1912e266c5af451b1f83
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-28 21:20:55 -0500

    :wrench: Add Redirects Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,51 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/twitter",
+        destination: "https://twitter.com/UltiRequiem",
+        permanent: true,
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/UltiRequiem",
+        permanent: true,
+      },
+      {
+        source: "/reddit",
+        destination: "https://reddit.com/u/UltiRequiem",
+        permanent: true,
+      },
+      {
+        source: "/youtube",
+        destination: "https://www.youtube.com/UltiRequiem",
+        permanent: true,
+      },
+      {
+        source: "/twitch",
+        destination: "https://twitch.tv/UltiRequiem",
+        permanent: true,
+      },
+      {
+        source: "/discord",
+        destination: "https://discord.gg/XW8mK5yu5R",
+        permanent: true,
+      },
+      {
+        source: "/twitter",
+        destination: "https://twitter.com/UltiRequiem",
+        permanent: true,
+      },
+      {
+        source: "/10",
+        destination: "https://youtu.be/G5HcvgepK-I",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
