96381cb5e8c8416d7f5c4275cf7f805cea0ca09f
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-28 21:10:28 -0500

    :wrench: Add tailwinds

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
+const config = {
+  content: ["./src/**/*.{tsx,ts,css}"],
+  darkMode: "media",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = config;
