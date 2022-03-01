aa75250b950cf3bec839e17ca48a098f3cf31f10
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-28 22:20:41 -0500

    :sparkles: Spotify Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,17 @@ import type { NextPage } from "next";
 
 import { age } from "utils";
 
+import { Song } from "components";
+
 const Home: NextPage = () => {
   return (
     <div>
       <p>Work in progress</p>
       <p>Go to my blog while in work on this</p>
       <p>https://blog.ultirequiem.com/</p>
-      
+
       <p>I'm a ~{age.toPrecision(10)} Year old software engineer from Peru</p>
+      <Song />
     </div>
   );
 };
