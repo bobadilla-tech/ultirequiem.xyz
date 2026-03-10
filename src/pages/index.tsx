c57a561d00fc5195045a5ec805e6c72b545c647e
UltiRequiem <eliaz.bobadilladev@gmail.com>
2026-03-10 13:20:40 -0500

    tweak: light mode styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -131,7 +131,7 @@ const Index: NextPage<Props> = (props) => {
 						target="_blank"
 						rel="noopener noreferrer"
 						title="X / Twitter"
-						className="inline-flex items-center justify-center w-11 h-11 rounded-xl bg-black hover:bg-gray-800 text-white transition-colors"
+						className="inline-flex items-center justify-center w-11 h-11 rounded-xl bg-black hover:bg-gray-900 text-white transition-colors dark:bg-white dark:text-black dark:hover:bg-gray-200"
 					>
 						<SiX className="w-5 h-5" />
 					</a>
